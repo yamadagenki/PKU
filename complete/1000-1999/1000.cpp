1e48f716ebbfa41d30c5e79e355764b63dbf615d
XuShengbo <xushengbo@XuShengbo-no-MacBook-Air.local>
2013-06-14 13:26:44 +0900

    add 1000.cpp

---
+++ complete/1000-1999/1000.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  TopCoderSample
+//
+//  Created by XuShengbo on 2013/04/19.
+//  Copyright (c) 2013年 XuShengbo. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
